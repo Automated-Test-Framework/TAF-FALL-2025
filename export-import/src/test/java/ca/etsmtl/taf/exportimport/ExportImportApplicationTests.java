78f9d636bb96e6ec7e02069368d1b7e2598aa9e8
Vincent Renaud <v.pisano35@gmail.com>
2025-10-03 15:15:13 -0400

    feat: add export-import module

---
+++ export-import/src/test/java/ca/etsmtl/taf/exportimport/ExportImportApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.etsmtl.taf.exportimport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExportImportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
