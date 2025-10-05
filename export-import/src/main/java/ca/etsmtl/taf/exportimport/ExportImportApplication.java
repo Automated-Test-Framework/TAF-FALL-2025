98c3f82e4c872e1d6a63ee403cf24c7cae6e7d7a
Pravdin Maksym <maks111111112@gmail.com>
2025-10-05 13:01:10 -0400

    feat: Added java binding to the spring application with test print

--- export-import/src/main/java/ca/etsmtl/taf/exportimport/ExportImportApplication.java
+++ export-import/src/main/java/ca/etsmtl/taf/exportimport/ExportImportApplication.java
@@ -1,12 +1,41 @@
 package ca.etsmtl.taf.exportimport;
 
+import com.gurock.testrail.APIClient;
+import com.gurock.testrail.APIException;
+import org.json.simple.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class ExportImportApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws APIException, IOException {
+
+        String url = System.getenv("TESTRAIL_URL");
+        String user = System.getenv("TESTRAIL_USER");
+        String apiKey = System.getenv("TESTRAIL_APIKEY");
+
+        APIClient client = new APIClient(url);
+        client.setUser(user);
+        client.setPassword(apiKey);
+
+        int caseId = 1;
+
+        try {
+            // Perform GET request
+            JSONObject caseData = (JSONObject) client.sendGet("get_case/" + caseId);
+            System.out.println("Title: " + caseData.get("title"));
+            System.out.println("Section ID: " + caseData.get("section_id"));
+            System.out.println("Created by: " + caseData.get("created_by"));
+
+        } catch (IOException | APIException e) {
+            e.printStackTrace();
+        }
+
         SpringApplication.run(ExportImportApplication.class, args);
     }
 
