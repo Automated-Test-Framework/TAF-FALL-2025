1f597a9809200ff0018cf70420f2ffdfaa0d85a4
injailoutsoon <46853989+injailoutsoon@users.noreply.github.com>
2025-10-22 00:33:19 -0400

    build(auth) - add local build config instead of using an image not reflecting any local code change

--- auth/src/main/java/ca/etsmtl/taf/auth/controller/AuthController.java
+++ auth/src/main/java/ca/etsmtl/taf/auth/controller/AuthController.java
@@ -15,63 +15,65 @@ import ca.etsmtl.taf.auth.payload.request.SignupRequest;
 import ca.etsmtl.taf.auth.payload.response.MessageResponse;
 import ca.etsmtl.taf.auth.payload.request.LoginRequest;
 import org.springframework.web.client.HttpClientErrorException;
-// import org.springframework.web.bind.annotation.CrossOrigin;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
-// @CrossOrigin(origins = "*")
 @RequestMapping("/api")
 public class AuthController {
 
-  @GetMapping("/")
-  public String greeting() {
-    return "Hello from Auth Microservice!";
-  }
-
-  @Autowired
-  private JwtService jwtService;
-
-  @Autowired
-  private UserOldService userService;
-
-  @PostMapping("/signin")
-  public ResponseEntity<JwtResponse> createAuthenticationToken(@RequestBody @Valid LoginRequest authenticationRequest) throws Exception {
-    final JwtResponse response = this.jwtService.createJwtToken(authenticationRequest);
-    //System.out.println("response");
-    //System.out.println(response.getFullName());
-    return ResponseEntity.ok(response);
-  }
-
-  @PostMapping("/validate-token")
-  public ResponseEntity<Boolean> validateToken(@RequestBody @Valid ValidateTokenRequest validateTokenRequest) throws Exception {
-    final boolean response = this.jwtService.validateJwtToken(validateTokenRequest);
-    return ResponseEntity.ok(response);
-  }
-
-  @PostMapping("/refresh-token")
-  public ResponseEntity<?> validateToken(@RequestBody @Valid RefreshTokenRequest refreshTokenRequest) throws Exception {
-    try{
-      final JwtResponse response = this.jwtService.refreshJwtToken(refreshTokenRequest);
-      return ResponseEntity.ok(response);
-    }catch(HttpClientErrorException err){
-      return ResponseEntity.status(err.getStatusCode()).body(err.getMessage());
-    }
-  }
-
-  @PostMapping("/signup")
-  public ResponseEntity<?> registerUser(@RequestBody SignupRequest request) {
-    if (this.userService.existsByUsername(request.getUsername())) {
-      return ResponseEntity.badRequest().body("Username is already taken.");
+    @GetMapping("/")
+    public String greeting() {
+        return "Hello from Auth Microservice!";
     }
 
-    if (this.userService.existsByEmail(request.getEmail())) {
-      return ResponseEntity
-              .badRequest()
-              .body(new MessageResponse("Error: Email is already in use!"));
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private UserOldService userService;
+
+    @PostMapping("/signin")
+    public ResponseEntity<JwtResponse> createAuthenticationToken(@RequestBody @Valid LoginRequest authenticationRequest) throws Exception {
+        final JwtResponse response = this.jwtService.createJwtToken(authenticationRequest);
+        log.info("User {} signed in successfully.", authenticationRequest.getUsername());
+        return ResponseEntity.ok(response);
     }
 
-    this.userService.save(request);
+    @PostMapping("/validate-token")
+    public ResponseEntity<Boolean> validateToken(@RequestBody @Valid ValidateTokenRequest validateTokenRequest) throws Exception {
+        final boolean response = this.jwtService.validateJwtToken(validateTokenRequest);
+        log.info("Token validation result: {}", response);
+        return ResponseEntity.ok(response);
+    }
 
-    return ResponseEntity.ok(new MessageResponse("Inscription Réussie.!"));
-  }
+    @PostMapping("/refresh-token")
+    public ResponseEntity<?> validateToken(@RequestBody @Valid RefreshTokenRequest refreshTokenRequest) throws Exception {
+        try {
+            final JwtResponse response = this.jwtService.refreshJwtToken(refreshTokenRequest);
+            log.info("Token refreshed successfully");
+            return ResponseEntity.ok(response);
+        } catch (HttpClientErrorException err) {
+            log.error("Error refreshing token: {}", err.getMessage());
+            return ResponseEntity.status(err.getStatusCode()).body(err.getMessage());
+        }
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@RequestBody SignupRequest request) {
+        if (this.userService.existsByUsername(request.getUsername())) {
+            log.info("Attempted registration with existing username: {}", request.getUsername());
+            return ResponseEntity.badRequest().body("Username is already taken.");
+        }
+
+        if (this.userService.existsByEmail(request.getEmail())) {
+            log.error("Attempted registration with existing email: {}", request.getEmail());
+            return ResponseEntity.badRequest().body(new MessageResponse("Error: Email is already in use!"));
+        }
+
+        this.userService.save(request);
+
+        return ResponseEntity.ok(new MessageResponse("Inscription Réussie.!"));
+    }
 
 }
